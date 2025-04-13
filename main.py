9ea70d15b7e77fbb25e9786af1a82c4b7619a9d1
KwachOjunga <ojungakwach@gmail.com>
2025-04-13 05:22:55 +0300

    hifmea ml - project

---
+++ main.py
@@ -0,0 +1,17 @@
+from sklearn.preprocessing import OneHotEncoder, LabelEncoder, OrdinalEncoder
+import numpy as np
+import polars as pl
+
+FILE_PATH = ""
+
+# read datasets
+file_n = pl.
+
+# perform data processing
+
+def main():
+    
+
+
+if __name__ == "__main__":
+    main()
