49de03af88c101db9b1f8d86f65a971969aa4370
KwachOjunga <ojungakwach@gmail.com>
2025-04-14 07:01:03 +0300

    rectify var assignment bug

--- main.py
+++ main.py
@@ -1,32 +1,34 @@
 from sklearn.preprocessing import OneHotEncoder, LabelEncoder, OrdinalEncoder
 import numpy as np
 import polars as pl
+import os
 
 FILE_PATH = ""
 
+
 # read datasets -- assuming they are all execel files
-def read_file_into_memory(file: str|list):
-    if type(file) == list:
-        n = 0 
+def read_file_into_memory(file: str | list):
+    if type(file) is list:
+        n = 0
         for i in file:
             if os.path.exists(i):
                 try:
-                   file_[n] =  pl.read_excel(i)
-                   n += 1 
+                    globals()[f"file_{n}"] = pl.read_excel(i)
+                    n += 1
                 except Exception as e:
-                   raise e
+                    raise e
     else:
         try:
             file_name = pl.read_excel(file)
-        except:
-            raise Exception
+        except Exception as e:
+            raise e
+
 
 # perform data processing
 
 
-
 def main():
-    
+    pass
 
 
 if __name__ == "__main__":
