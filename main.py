43a130de7b05ef5923fdcd07effcc5cf24d99f21
KwachOjunga <ojungakwach@gmail.com>
2025-04-14 05:49:31 +0300

    enable reading a file

--- main.py
+++ main.py
@@ -4,10 +4,26 @@ import polars as pl
 
 FILE_PATH = ""
 
-# read datasets
-file_n = pl.
+# read datasets -- assuming they are all execel files
+def read_file_into_memory(file: str|list):
+    if type(file) == list:
+        n = 0 
+        for i in file:
+            if os.path.exists(i):
+                try:
+                   file_[n] =  pl.read_excel(i)
+                   n += 1 
+                except Exception as e:
+                   raise e
+    else:
+        try:
+            file_name = pl.read_excel(file)
+        except:
+            raise Exception
 
 # perform data processing
+
+
 
 def main():
     
