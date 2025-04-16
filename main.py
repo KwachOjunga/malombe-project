dbf4fd0172fbfeda833c6f74afdaf35a4898a4c0
KwachOjunga <ojungakwach@gmail.com>
2025-04-16 08:16:11 +0300

    improve error handling

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from sklearn.preprocessing import OneHotEncoder, LabelEncoder, OrdinalEncoder
+from pathlib import Path
 import numpy as np
 import polars as pl
-import os
+import sys
 
 FILE_PATH = ""
+file_name = ""
 
 
 # read datasets -- assuming they are all execel files
@@ -11,25 +13,30 @@ def read_file_into_memory(file: str | list):
     if type(file) is list:
         n = 0
         for i in file:
-            if os.path.exists(i):
+            if Path(f"{i}").exists():
                 try:
                     globals()[f"file_{n}"] = pl.read_excel(i)
                     n += 1
                 except Exception as e:
                     raise e
+            else:
+                raise FileNotFoundError
     else:
-        try:
-            file_name = pl.read_excel(file)
-        except Exception as e:
-            raise e
+        if not Path(f"{file}").exists():
+            raise FileNotFoundError
+        else:
+            try:
+                globals()[file_name] = pl.read_excel(file)
+            except Exception as e:
+                raise e
 
 
 # perform data processing
 
 
-def main():
-    pass
+# def main():
+#     pass
 
 
 if __name__ == "__main__":
-    main()
+    read_file_into_memory(sys.argv[1])
